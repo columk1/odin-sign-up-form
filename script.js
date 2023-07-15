f194344cbb3436c5a4f319db441b8f590eb2e745
Colum Kelly <columk1@users.noreply.github.com>
2023-07-14 23:59:01 -0700

    Create layout Add styles Add CSS validation Add script for password matching

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let password = document.getElementById("password")
+  let confirm_password = document.getElementById("confirm_password");
+
+function validatePassword() {
+  if(password.value != confirm_password.value) {
+    password.setCustomValidity("Passwords Don't Match");
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+    password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
